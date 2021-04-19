a2fc39e6539ae8a30c0f4c87a514a77a3fe880e7
tammergalal <tammer.galal@datadoghq.com>
2021-04-19 12:51:07 -0400

    modifying my yaml to add service and version tagging

--- blue-green-deploy-tracking/foreground.sh
+++ blue-green-deploy-tracking/foreground.sh
@@ -5,6 +5,5 @@ while [ ! `k get nodes 2>/dev/null | wc -l ` -eq 2 ]; do
   sleep 0.3
 done
 kubectl completion bash >/etc/bash_completion.d/kubectl
-complete -F __start_kubectl k
 clear
 prepenvironment
