ef81deaff541ff36ae05c8e469076318e0793320
tammergalal <tammer.galal@datadoghq.com>
2021-04-19 19:01:53 -0400

    changing tactics

--- blue-green-deploy-tracking/background.sh
+++ blue-green-deploy-tracking/background.sh
@@ -3,13 +3,6 @@ curl -s https://datadoghq.dev/katacodalabtools/r?raw=true|bash
 touch status.txt
 echo "">/root/status.txt
 
-curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
-chmod 700 get_helm.sh
-./get_helm.sh
-# helm repo add stable https://kubernetes-charts.storage.googleapis.com
-helm repo add datadog https://helm.datadoghq.com
-helm repo add stable https://charts.helm.sh/stable
-helm repo update
 # echo "Waiting for kubernetes to start" >>/root/status.txt
 statusupdate "Waiting for kubernetes to start"
 while [ "$( kubectl get nodes --no-headers 2>/dev/null | wc -l )" != "2" ]; do
@@ -22,6 +15,9 @@ while [ "$( kubectl get nodes --no-headers 2>/dev/null| awk '{print $2}'|xargs )
 done
 # echo "Kubernetes ready.">>/root/status.txt
 statusupdate "Kubernetes ready!"
+
+kubectl create -f "https://raw.githubusercontent.com/DataDog/datadog-agent/master/Dockerfiles/manifests/cluster-agent/cluster-agent-rbac.yaml"
+kubectl create -f "https://raw.githubusercontent.com/DataDog/datadog-agent/master/Dockerfiles/manifests/cluster-agent/rbac.yaml"
 
 kubectl create secret generic datadog-api --from-literal=token=$DD_API_KEY
 
