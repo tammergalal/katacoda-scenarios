2b099179bf5d32f5f6c86a5359bc2625f888ddf3
tammergalal <tammer.galal@datadoghq.com>
2021-04-20 10:40:39 -0400

    foreground/background.sh

--- blue-green-deploy-tracking/background.sh
+++ blue-green-deploy-tracking/background.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
 curl -s https://datadoghq.dev/katacodalabtools/r?raw=true|bash
-touch status.txt
-echo ""> /root/status.txt
-kubeloopstart=`date +%s`
-do
-  kubeloopend=`date +%s`
-  kubeloopruntime=$((kubeloopend-kubeloopstart))
-  echo "kubectl isn't ready yet."
-  echo "It has been $kubeloopruntime seconds"
-  echo "If this doesn't resolve after 60 seconds, contact support."
-  sleep 2
-done
-statusupdate "Kubernetes ready!"
-
-kubectl create -f "https://raw.githubusercontent.com/DataDog/datadog-agent/master/Dockerfiles/manifests/cluster-agent/cluster-agent-rbac.yaml"
-kubectl create -f "https://raw.githubusercontent.com/DataDog/datadog-agent/master/Dockerfiles/manifests/cluster-agent/rbac.yaml"
-
-statusupdate "RBACs complete"
 
 kubectl create secret generic datadog-api --from-literal=token=$DD_API_KEY
 
--- blue-green-deploy-tracking/foreground.sh
+++ blue-green-deploy-tracking/foreground.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 while [ ! -f "/usr/local/bin/prepenvironment" ]; do
   sleep 0.3
 done
