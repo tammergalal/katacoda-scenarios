6b9ed1a7893745761bb8a3eccfcfbe9ba8a075a8
tammergalal <tammer.galal@datadoghq.com>
2021-04-20 11:14:00 -0400

    foreground/background.sh

--- blue-green-deploy-tracking/background.sh
+++ blue-green-deploy-tracking/background.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 curl -s https://datadoghq.dev/katacodalabtools/r?raw=true|bash
 
-kubectl create secret generic datadog-api --from-literal=token=$DD_API_KEY
+kubeloopstart=`date +%s`
+until kubectl create secret generic datadog-api --from-literal=token=$DD_API_KEY
+do
+  kubeloopend=`date +%s`
+  kubeloopruntime=$((kubeloopend-kubeloopstart))
+  echo "kubectl isn't ready yet."
+  echo "It has been $kubeloopruntime seconds"
+  echo "If this doesn't resolve after 60 seconds, contact support."
+  sleep 2
+done
 
-statusupdate "Secrets created, starting services..."
+statusupdate "Kubernetes setup complete and secrets created, starting services..."
 
 kubectl apply -f k8s-yaml-files/db.yaml
 kubectl apply -f k8s-yaml-files/advertisements.yaml
