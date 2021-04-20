3c88f5774de559991e3b88c94fd09067a59b9b37
tammergalal <tammer.galal@datadoghq.com>
2021-04-20 10:55:05 -0400

    foreground/background.sh

--- blue-green-deploy-tracking/foreground.sh
+++ blue-green-deploy-tracking/foreground.sh
@@ -2,19 +2,6 @@
 while [ ! -f "/usr/local/bin/prepenvironment" ]; do
   sleep 0.3
 done
-sleep 0.3
-
-kubeloopstart=`date +%s`
-
-do
-  kubeloopend=`date +%s`
-  kubeloopruntime=$((kubeloopend-kubeloopstart))
-  echo "kubectl isn't ready yet."
-  echo "It has been $kubeloopruntime seconds"
-  echo "If this doesn't resolve after 60 seconds, contact support."
-  sleep 2
-done
-
-clear
+clear 
 
 prepenvironment
