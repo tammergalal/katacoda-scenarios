70cf82bcd01ee9fe09f79be60076f8d4467727fe
tammergalal <tammer.galal@datadoghq.com>
2021-04-21 14:05:17 -0400

    fixing

--- blue-green-deploy-tracking/foreground.sh
+++ blue-green-deploy-tracking/foreground.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-launch.sh
-
-while [ ! `ls -l /root/k8s-yaml-files/*.yaml 2>/dev/null | wc -l ` -eq 10 ]; do
+while [ ! `ls -l /root/k8s-yaml-files/*.yaml 2>/dev/null | wc -l ` -eq 8 ]; do
   sleep 0.3
 done
 while [ ! `k get nodes 2>/dev/null | wc -l ` -eq 2 ]; do
