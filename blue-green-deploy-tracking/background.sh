6a76aa56e83a1f23d0391cfb26dffffbd8a52350
tammergalal <tammer.galal@datadoghq.com>
2021-04-21 14:39:01 -0400

    working on better setup

--- blue-green-deploy-tracking/background.sh
+++ blue-green-deploy-tracking/background.sh
@@ -23,7 +23,7 @@ done
 # echo "Kubernetes ready.">>/root/status.txt
 statusupdate "Kubernetes ready."
 
-git clone git@github.com:DataDog/ecommerce-workshop.git
+git clone https://github.com/DataDog/ecommerce-workshop.git
 
 kubectl create secret generic datadog-api --from-literal=token=$DD_API_KEY
 
