54ad10549e299af2b6edfb55cdeb7cd778ed1bd4
tammergalal <tammer.galal@datadoghq.com>
2021-04-21 17:06:31 -0400

    frontend not working

--- blue-green-deploy-tracking/background.sh
+++ blue-green-deploy-tracking/background.sh
@@ -23,14 +23,14 @@ done
 # echo "Kubernetes ready.">>/root/status.txt
 statusupdate "Kubernetes ready."
 
-git clone https://github.com/DataDog/ecommerce-workshop.git
-
 kubectl create secret generic datadog-api --from-literal=token=$DD_API_KEY
 
 kubectl apply -f k8s-yaml-files/db.yaml
 kubectl apply -f k8s-yaml-files/advertisements.yaml
 kubectl apply -f k8s-yaml-files/discounts.yaml
 kubectl apply -f k8s-yaml-files/frontend.yaml
+
+git clone https://github.com/DataDog/ecommerce-workshop.git
 
 # wait-for-it --timeout=300 localhost:3000
 
