f4c79fe3b5969ea30cb16341e326856ed346d4b3
tammergalal <tammer.galal@datadoghq.com>
2021-04-20 21:16:18 -0400

    changin foreground

--- blue-green-deploy-tracking/foreground.sh
+++ blue-green-deploy-tracking/foreground.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 launch.sh
+while [ ! `ls -l /root/k8s-yaml-files/*.yaml 2>/dev/null | wc -l ` -eq 10 ]; do
+  sleep 0.3
+done
+while [ ! `k get nodes 2>/dev/null | wc -l ` -eq 2 ]; do
+  sleep 0.3
+done
 
 kubectl completion bash >/etc/bash_completion.d/kubectl
 complete -F __start_kubectl k
