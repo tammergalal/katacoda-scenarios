e2bc90b2b5d8d7406e3d4cbd8303e854c19820ef
tammergalal <tammer.galal@datadoghq.com>
2021-04-21 13:58:41 -0400

    changing image

--- blue-green-deploy-tracking/background.sh
+++ blue-green-deploy-tracking/background.sh
@@ -3,7 +3,7 @@ curl -s https://datadoghq.dev/katacodalabtools/r?raw=true|bash
 touch status.txt
 echo "">/root/status.txt
 
-apt-get install wait-for-it
+# apt-get install wait-for-it
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
 chmod 700 get_helm.sh
 ./get_helm.sh
@@ -30,7 +30,7 @@ kubectl apply -f k8s-yaml-files/advertisements.yaml
 kubectl apply -f k8s-yaml-files/discounts.yaml
 kubectl apply -f k8s-yaml-files/frontend.yaml
 
-wait-for-it --timeout=300 localhost:3000
+# wait-for-it --timeout=300 localhost:3000
 
 statusupdate complete
 
