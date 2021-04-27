809b3db41b0955ac969c161b784d2eba0078e4d2
tammergalal <tammer.galal@datadoghq.com>
2021-04-27 11:24:25 -0400

    fixing background error

--- blue-green-deploy-tracking/background.sh
+++ blue-green-deploy-tracking/background.sh
@@ -32,12 +32,12 @@ cp /root/ecommerce-workshop/discounts-service-fixed/discounts.py /root/discounts
 
 cp /root/ecommerce-workshop/discounts-service-fixed/discounts.py /root/discounts_1_2.py
 
-sudo sed -ie '8i  \ \ \ \ tags.datadoghq.com/service: '\''advertisements'\''\n \ \ \ tags.datadoghq.com/version: '\''1.0'\''' /root/k8s-yaml-files/advertisements.yaml
-sudo sed -ie '49i \ \ \ \ \ \ \ \ \ \ - name: DD_SERVICE\n  \ \ \ \ \ \ \ \ \ \ valueFrom:\n \ \ \ \ \ \ \ \ \ \ \ \ \ fieldRef:\n \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ fieldPath: metadata.labels['\''tags.datadoghq.com/service'\'']\n \ \ \ \ \ \ \ \ \ - name: DD_VERSION\n  \ \ \ \ \ \ \ \ \ \ valueFrom:\n \ \ \ \ \ \ \ \ \ \ \ \ \ fieldRef:\n \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ fieldPath: metadata.labels['\''tags.datadoghq.com/version'\'']' /root/k8s-yaml-files/advertisements.yaml
-sudo sed -ie '8i  \ \ \ \ tags.datadoghq.com/service: '\''discounts'\''\n \ \ \ tags.datadoghq.com/version: '\''1.0'\''' /root/k8s-yaml-files/discounts.yaml
-sudo sed -ie '49i \ \ \ \ \ \ \ \ \ \ - name: DD_SERVICE\n  \ \ \ \ \ \ \ \ \ \ valueFrom:\n \ \ \ \ \ \ \ \ \ \ \ \ \ fieldRef:\n \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ fieldPath: metadata.labels['\''tags.datadoghq.com/service'\'']\n \ \ \ \ \ \ \ \ \ - name: DD_VERSION\n  \ \ \ \ \ \ \ \ \ \ valueFrom:\n \ \ \ \ \ \ \ \ \ \ \ \ \ fieldRef:\n \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ fieldPath: metadata.labels['\''tags.datadoghq.com/version'\'']' /root/k8s-yaml-files/discounts.yaml
-sudo sed -ie '29d' /root/discounts_1_1.py
-sudo sed -ie '42d' /root/discounts_1_1.py
+sudo sed -i '8i  \ \ \ \ tags.datadoghq.com/service: '\''advertisements'\''\n \ \ \ tags.datadoghq.com/version: '\''1.0'\''' /root/k8s-yaml-files/advertisements.yaml
+sudo sed -i '49i \ \ \ \ \ \ \ \ \ \ - name: DD_SERVICE\n  \ \ \ \ \ \ \ \ \ \ valueFrom:\n \ \ \ \ \ \ \ \ \ \ \ \ \ fieldRef:\n \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ fieldPath: metadata.labels['\''tags.datadoghq.com/service'\'']\n \ \ \ \ \ \ \ \ \ - name: DD_VERSION\n  \ \ \ \ \ \ \ \ \ \ valueFrom:\n \ \ \ \ \ \ \ \ \ \ \ \ \ fieldRef:\n \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ fieldPath: metadata.labels['\''tags.datadoghq.com/version'\'']' /root/k8s-yaml-files/advertisements.yaml
+sudo sed -i '8i  \ \ \ \ tags.datadoghq.com/service: '\''discounts'\''\n \ \ \ tags.datadoghq.com/version: '\''1.0'\''' /root/k8s-yaml-files/discounts.yaml
+sudo sed -i '49i \ \ \ \ \ \ \ \ \ \ - name: DD_SERVICE\n  \ \ \ \ \ \ \ \ \ \ valueFrom:\n \ \ \ \ \ \ \ \ \ \ \ \ \ fieldRef:\n \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ fieldPath: metadata.labels['\''tags.datadoghq.com/service'\'']\n \ \ \ \ \ \ \ \ \ - name: DD_VERSION\n  \ \ \ \ \ \ \ \ \ \ valueFrom:\n \ \ \ \ \ \ \ \ \ \ \ \ \ fieldRef:\n \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ fieldPath: metadata.labels['\''tags.datadoghq.com/version'\'']' /root/k8s-yaml-files/discounts.yaml
+sudo sed -i '29d' /root/discounts_1_1.py
+sudo sed -i '42d' /root/discounts_1_1.py
 
 kubectl create secret generic datadog-api --from-literal=token=$DD_API_KEY
 
