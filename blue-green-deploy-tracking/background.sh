333a9671cca5aa412c08bb1efd183262ec9f6993
tammergalal <tammer.galal@datadoghq.com>
2021-04-27 09:30:21 -0400

    working on background/foreground

--- blue-green-deploy-tracking/background.sh
+++ blue-green-deploy-tracking/background.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+mkdir k8s-yaml-files
+
 curl -s https://datadoghq.dev/katacodalabtools/r?raw=true|bash
 
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
@@ -16,23 +18,7 @@ while [ "$( kubectl get nodes --no-headers 2>/dev/null | wc -l )" != "2" ]; do
   sleep 1
 done
 
-mkdir k8s-yaml-files
-
 git clone https://github.com/DataDog/ecommerce-workshop.git 
-
-cp /root/ecommerce-workshop/deploy/generic-k8s/ecommerce-app/advertisements.yaml /root/k8s-yaml-files
-cp /root/ecommerce-workshop/deploy/generic-k8s/ecommerce-app/discounts.yaml /root/k8s-yaml-files
-cp /root/ecommerce-workshop/deploy/generic-k8s/ecommerce-app/discounts.yaml /root/k8s-yaml-files/discounts2.yaml
-cp /root/ecommerce-workshop/deploy/generic-k8s/ecommerce-app/frontend.yaml /root/k8s-yaml-files
-cp /root/ecommerce-workshop/deploy/generic-k8s/ecommerce-app/db.yaml /root/k8s-yaml-files
-cp /root/ecommerce-workshop/discounts-service-fixed/discounts.py /root/discounts_1_1.py
-
-sudo sed -ie '8i  \ \ \ \ tags.datadoghq.com/service: '\''advertisements'\''\n \ \ \ tags.datadoghq.com/version: '\''1.0'\''' /root/k8s-yaml-files/advertisements.yaml
-sudo sed -ie '49i \ \ \ \ \ \ \ \ \ \ - name: DD_SERVICE\n  \ \ \ \ \ \ \ \ \ \ valueFrom:\n \ \ \ \ \ \ \ \ \ \ \ \ \ fieldRef:\n \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ fieldPath: metadata.labels['\''tags.datadoghq.com/service'\'']\n \ \ \ \ \ \ \ \ \ - name: DD_VERSION\n  \ \ \ \ \ \ \ \ \ \ valueFrom:\n \ \ \ \ \ \ \ \ \ \ \ \ \ fieldRef:\n \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ fieldPath: metadata.labels['\''tags.datadoghq.com/version'\'']' /root/k8s-yaml-files/advertisements.yaml
-sudo sed -ie '8i  \ \ \ \ tags.datadoghq.com/service: '\''discounts'\''\n \ \ \ tags.datadoghq.com/version: '\''1.0'\''' /root/k8s-yaml-files/discounts.yaml
-sudo sed -ie '49i \ \ \ \ \ \ \ \ \ \ - name: DD_SERVICE\n  \ \ \ \ \ \ \ \ \ \ valueFrom:\n \ \ \ \ \ \ \ \ \ \ \ \ \ fieldRef:\n \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ fieldPath: metadata.labels['\''tags.datadoghq.com/service'\'']\n \ \ \ \ \ \ \ \ \ - name: DD_VERSION\n  \ \ \ \ \ \ \ \ \ \ valueFrom:\n \ \ \ \ \ \ \ \ \ \ \ \ \ fieldRef:\n \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ fieldPath: metadata.labels['\''tags.datadoghq.com/version'\'']' /root/k8s-yaml-files/discounts.yaml
-sudo sed -ie '29d' /root/discounts_1_1.py
-sudo sed -ie '42d' /root/discounts_1_1.py
 
 kubectl create secret generic datadog-api --from-literal=token=$DD_API_KEY
 
