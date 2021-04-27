a7bb3716b77729482b9b5c9b81dc6cc06f1849db
tammergalal <tammer.galal@datadoghq.com>
2021-04-27 10:56:33 -0400

    working on background/foreground

--- blue-green-deploy-tracking/background.sh
+++ blue-green-deploy-tracking/background.sh
@@ -24,19 +24,14 @@ mkdir k8s-yaml-files
 
 git clone https://github.com/DataDog/ecommerce-workshop.git 
 
-cp /root/ecommerce-workshop/deploy/generic-k8s/ecommerce-app/advertisements.yaml /root/k8s-yaml-files/advertisements.yaml
-
-cp /root/ecommerce-workshop/deploy/generic-k8s/ecommerce-app/discounts.yaml /root/k8s-yaml-files/discounts.yaml
-
-cp /root/ecommerce-workshop/deploy/generic-k8s/ecommerce-app/frontend.yaml /root/k8s-yaml-files/frontend.yaml
-
-cp /root/ecommerce-workshop/deploy/generic-k8s/ecommerce-app/db.yaml /root/k8s-yaml-files/db.yaml
+cp /root/ecommerce-workshop/deploy/generic-k8s/ecommerce-app/. /root./k8s-yaml-files
 
 cp /root/ecommerce-workshop/deploy/generic-k8s/ecommerce-app/discounts.yaml /root/k8s-yaml-files/discounts2.yaml
 
 cp /root/ecommerce-workshop/discounts-service-fixed/discounts.py /root/discounts_1_1.py
 
 cp /root/ecommerce-workshop/discounts-service-fixed/discounts.py /root/discounts_1_2.py
+
 sudo sed -ie '8i  \ \ \ \ tags.datadoghq.com/service: '\''advertisements'\''\n \ \ \ tags.datadoghq.com/version: '\''1.0'\''' /root/k8s-yaml-files/advertisements.yaml
 sudo sed -ie '49i \ \ \ \ \ \ \ \ \ \ - name: DD_SERVICE\n  \ \ \ \ \ \ \ \ \ \ valueFrom:\n \ \ \ \ \ \ \ \ \ \ \ \ \ fieldRef:\n \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ fieldPath: metadata.labels['\''tags.datadoghq.com/service'\'']\n \ \ \ \ \ \ \ \ \ - name: DD_VERSION\n  \ \ \ \ \ \ \ \ \ \ valueFrom:\n \ \ \ \ \ \ \ \ \ \ \ \ \ fieldRef:\n \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ fieldPath: metadata.labels['\''tags.datadoghq.com/version'\'']' /root/k8s-yaml-files/advertisements.yaml
 sudo sed -ie '8i  \ \ \ \ tags.datadoghq.com/service: '\''discounts'\''\n \ \ \ tags.datadoghq.com/version: '\''1.0'\''' /root/k8s-yaml-files/discounts.yaml
