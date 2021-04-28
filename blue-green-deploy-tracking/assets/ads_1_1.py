74a5bc2587b01c5c7794074354958f7a87c80960
tammergalal <tammer.galal@datadoghq.com>
2021-04-28 14:29:55 -0400

    steps fix

--- blue-green-deploy-tracking/assets/ads_1_1.py
+++ blue-green-deploy-tracking/assets/ads_1_1.py
@@ -4,8 +4,8 @@ import time
 
 from random_word import RandomWords
 
-from flask imprt Flask, Response, jsonify, send_from_directory
-from flask imprt request as flask_request
+from flask import Flask, Response, jsonify, send_from_directory
+from flask import request as flask_request
 
 from bootstrap import create_app
 from models import Advertisement, db
@@ -20,11 +20,59 @@ def hello():
     app.logger.info("home url for ads called")
     return Response({'Hello from Advertisements!': 'world'}, mimetype='application/json')
 
+@app.route('/banners/<path:banner>')
+def banner_image(banner):
+    app.logger.info(f"attempting to grab banner at {banner}")
+    return send_from_directory('ads', banner)
+
 @app.route('/weighted-banners/<float:weight>')
 def weighted_image(weight):
     app.logger.info(f"attempting to grab banner weight of less than {weight}")
-    advertisements = Advertisement.query.all()
+
     for ad in advertisements:
         if ad.weight < weight:
             return jsonify(ad.serialize())
 
+@app.route('/ads', methods=['GET', 'POST'])
+def status():
+    if flask_request.method == 'GET':
+
+        try:
+
+            app.logger.info(f"Total advertisements available: {len(advertisements)}")
+            # adding a half sleep to test something
+            time.sleep(2.5)
+            return jsonify([b.serialize() for b in advertisments])
+
+        except:
+            app.logger.error("An error occured while getting ad.")
+            err = jsonify({'error': 'Internal Server Error'})
+            err.status_code = 500
+            return err
+
+    elif flask_request.method == 'POST':
+
+        try:
+            # create a new advertisement with random name and value
+            new_advertisement = Advertiseent('Advertisement ' + str(discounts_count + 1),
+                                    '/',
+                                    random.randint(10,500))
+            app.logger.info(f"Adding advertisement {new_advertisement}")
+            db.session.add(new_advertisement)
+            db.session.commit()
+
+            # adding a half sleep to test something
+            time.sleep(2.5)
+            return jsonify([b.serialize() for b in advertements])
+
+        except:
+
+            app.logger.error("An error occured while creating a new ad.")
+            err = jsonify({'error': 'Internal Server Error'})
+            err.status_code = 500
+            return err
+
+    else:
+        err = jsonify({'error': 'Invalid request method'})
+        err.status_code = 405
+        return err
