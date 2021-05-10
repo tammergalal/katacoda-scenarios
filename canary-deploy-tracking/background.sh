af0116822c9db4b1207fc1bfcd6aee3c4aff245a
tammergalal <tammer.galal@datadoghq.com>
2021-05-10 11:45:28 -0400

    changing background based on feedback

--- canary-deploy-tracking/background.sh
+++ canary-deploy-tracking/background.sh
@@ -9,7 +9,7 @@ STATUS=$(cat /root/status.txt)
 if [ "$STATUS" != "complete" ]; then
   echo ""> /root/status.txt
 
-  wall-n "Installing Helm and cloning necessary materials"
+  wall -n "Installing Helm and cloning necessary materials"
 
   curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
   chmod 700 get_helm.sh
@@ -33,12 +33,12 @@ if [ "$STATUS" != "complete" ]; then
   kubectl apply -f k8s-yaml-files/discounts.yaml
   kubectl apply -f k8s-yaml-files/frontend.yaml
 
-  while [ "$NPODS" != "5" ]; do
+  while [ "$NPODS" != "4" ]; do
     sleep 0.3
     NPODS=$(kubectl get pods --field-selector=status.phase=Running | grep -v NAME | wc -l)
   done
 
-  statusupdate complete
+  echo "complete">>/root/status.txt
 fi
 
 
