0f30dfda4e91e63fcb8984fc054cee8f042d276c
tammergalal <tammer.galal@datadoghq.com>
2021-05-10 10:42:45 -0400

    waitfork8s update

--- canary-deploy-tracking/background.sh
+++ canary-deploy-tracking/background.sh
@@ -14,16 +14,7 @@ helm repo update
 
 git clone https://github.com/DataDog/ecommerce-workshop.git
 
-until kubectl apply -f k8s-yaml-files/db.yaml
-do
-  kubeloopend=`date +%s`
-  kubeloopruntime=$((kubeloopend-kubeloopstart))
-  echo "kubectl isn't ready yet."
-  echo "It has been $kubeloopruntime seconds"
-  echo "If this doesn't resolve after 60 seconds, contact support."
-  sleep 2
-done
-
+kubectl apply -f k8s-yaml-files/db.yaml
 kubectl apply -f k8s-yaml-files/advertisements.yaml
 kubectl apply -f k8s-yaml-files/discounts.yaml
 kubectl apply -f k8s-yaml-files/frontend.yaml
