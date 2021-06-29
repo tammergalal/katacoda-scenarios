de1e9f57993488ed9ad406247a98ec4e82fa0195
tammergalal <tammer.galal@datadoghq.com>
2021-06-29 12:17:01 -0400

    modifying background

--- enrich-ootb-apm/background.sh
+++ enrich-ootb-apm/background.sh
@@ -2,4 +2,6 @@
 
 curl -s https://datadoghq.dev/katacodalabtools/r?raw=true|bash
 
+git clone https://github.com/DataDog/
+
 /ecommworkshop/gor --input-file-loop --input-file /ecommworkshop/requests_0.gor --output-http "http://localhost:3000" >> /dev/null 2>&1
