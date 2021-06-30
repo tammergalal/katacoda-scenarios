c1dd1b3eedc3b1f7e6c3630fd7aa959d5a5e00aa
tammergalal <tammer.galal@datadoghq.com>
2021-06-30 18:03:01 -0400

    modify background

--- enrich-ootb-apm/background.sh
+++ enrich-ootb-apm/background.sh
@@ -2,10 +2,22 @@
 
 curl -s https://datadoghq.dev/katacodalabtools/r?raw=true|bash
 
-git clone https://github.com/DataDog/ecommerce-workshop.git
-cd /ecommworkshop/docker-compose-files
+touch /root/status.txt
+sleep 1
+STATUS=$(cat /root/status.txt)
 
-sudo sed -i '37i \ \ \ \ volumes:\n \ \ \ \ \ - "../../discounts-service-fixed:/app"' /root/ecommerce-workshop/deploy/docker-compose/docker-compose-fixed-instrumented.yml
-sudo sed -i '84i \ \ \ \ volumes:\n \ \ \ \ \ - "../../ads-service-fixed:/app"' /root/ecommerce-workshop/deploy/docker-compose/docker-compose-fixed-instrumented.yml
+if [ "$STATUS" != "complete" ]; then
+  echo ""> /root/status.txt
+  
+  wall -n "Cloning necessary materials"
+
+  git clone https://github.com/DataDog/ecommerce-workshop.git
+  cd /ecommworkshop/docker-compose-files
+  sudo sed -i '37i \ \ \ \ volumes:\n \ \ \ \ \ - "../../discounts-service-fixed:/app"' /root/ecommerce-workshop/deploy/docker-compose/docker-compose-fixed-instrumented.yml
+  sudo sed -i '84i \ \ \ \ volumes:\n \ \ \ \ \ - "../../ads-service-fixed:/app"' /root/ecommerce-workshop/deploy/docker-compose/docker-compose-fixed-instrumented.yml
+
+  echo "complete"> /root/status.txt
+fi
+
 
 /ecommworkshop/gor --input-file-loop --input-file /ecommworkshop/requests_0.gor --output-http "http://localhost:3000" >> /dev/null 2>&1
