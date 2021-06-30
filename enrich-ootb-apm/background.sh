db12240d93e8ba98344435d8adef42e4fc6299bf
tammergalal <tammer.galal@datadoghq.com>
2021-06-30 17:52:00 -0400

    modify background

--- enrich-ootb-apm/background.sh
+++ enrich-ootb-apm/background.sh
@@ -5,7 +5,7 @@ curl -s https://datadoghq.dev/katacodalabtools/r?raw=true|bash
 git clone https://github.com/DataDog/ecommerce-workshop.git
 cd /ecommworkshop/docker-compose-files
 
-sudo sed -i '37i \ \ \ \ volumes:\n \ \ \ \ \ - "../../discounts-service-fixed:/app"' /root/ecommworkshop/deploy/docker-compose/docker-compose-fixed-instrumented.yml
-sudo sed -i '84i \ \ \ \ volumes:\n \ \ \ \ \ - "../../ads-service-fixed:/app"' /root/ecommworkshop/deploy/docker-compose/docker-compose-fixed-instrumented.yml
+sudo sed -i '37i \ \ \ \ volumes:\n \ \ \ \ \ - "../../discounts-service-fixed:/app"' /root/ecommerce-workshop/deploy/docker-compose/docker-compose-fixed-instrumented.yml
+sudo sed -i '84i \ \ \ \ volumes:\n \ \ \ \ \ - "../../ads-service-fixed:/app"' /root/ecommerce-workshop/deploy/docker-compose/docker-compose-fixed-instrumented.yml
 
 /ecommworkshop/gor --input-file-loop --input-file /ecommworkshop/requests_0.gor --output-http "http://localhost:3000" >> /dev/null 2>&1
