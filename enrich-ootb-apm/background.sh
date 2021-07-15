964cf3a5065633243a87def4c8072c81bce9fe53
tammergalal <tammer.galal@datadoghq.com>
2021-07-15 13:08:03 -0400

    changing to base-v3

--- enrich-ootb-apm/background.sh
+++ enrich-ootb-apm/background.sh
@@ -12,10 +12,9 @@ if [ "$STATUS" != "complete" ]; then
   git clone https://github.com/DataDog/ecommerce-workshop.git
   cd /ecommworkshop/docker-compose-files
   sudo sed -i '38i \ \ \ \ volumes:\n \ \ \ \ \ - "../../discounts-service-fixed:/app"' /root/ecommerce-workshop/deploy/docker-compose/docker-compose-fixed-instrumented.yml
-  sudo sed -i '85i \ \ \ \ volumes:\n \ \ \ \ \ - "../../ads-service-fixed:/app"' /root/ecommerce-workshop/deploy/docker-compose/docker-compose-fixed-instrumented.yml
+  sudo sed -i '86i \ \ \ \ volumes:\n \ \ \ \ \ - "../../ads-service-fixed:/app"' /root/ecommerce-workshop/deploy/docker-compose/docker-compose-fixed-instrumented.yml
 
   echo "complete"> /root/status.txt
 fi
-
 
 /ecommworkshop/gor --input-file-loop --input-file /ecommworkshop/requests_0.gor --output-http "http://localhost:3000" >> /dev/null 2>&1
