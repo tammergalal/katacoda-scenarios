d9b908a4e527f38e5ef703dab0c230a44e0e19a7
tammergalal <tammer.galal@datadoghq.com>
2021-07-27 14:30:32 -0400

    problems with starting up app on scenario startup

--- enrich-ootb-apm/background.sh
+++ enrich-ootb-apm/background.sh
@@ -2,16 +2,11 @@
 
 curl -s https://datadoghq.dev/katacodalabtools/r?raw=true|bash
 
-touch /root/status.txt
-STATUS=$(cat /root/status.txt)
+git clone https://github.com/DataDog/ecommerce-workshop.git
 
-if [ "$STATUS" != "complete" ]; then
-  echo ""> /root/status.txt
+sudo sed -i '38i \ \ \ \ volumes:\n \ \ \ \ \ - "../../discounts-service-fixed:/app"' /root/ecommerce-workshop/deploy/docker-compose/docker-compose-fixed-instrumented.yml
+sudo sed -i '84i \ \ \ \ volumes:\n \ \ \ \ \ - "../../ads-service-fixed:/app"' /root/ecommerce-workshop/deploy/docker-compose/docker-compose-fixed-instrumented.yml
 
-  git clone https://github.com/DataDog/ecommerce-workshop.git
-  sudo sed -i '38i \ \ \ \ volumes:\n \ \ \ \ \ - "../../discounts-service-fixed:/app"' /root/ecommerce-workshop/deploy/docker-compose/docker-compose-fixed-instrumented.yml
-  sudo sed -i '84i \ \ \ \ volumes:\n \ \ \ \ \ - "../../ads-service-fixed:/app"' /root/ecommerce-workshop/deploy/docker-compose/docker-compose-fixed-instrumented.yml
-  echo "complete"> /root/status.txt
-fi
+cd /ecommerce-workshop/deploy
 
 /ecommerce-workshop/gor --input-file-loop --input-file /ecommworkshop/requests_0.gor --output-http "http://localhost:3000" >> /dev/null 2>&1
--- enrich-ootb-apm/foreground.sh
+++ enrich-ootb-apm/foreground.sh
@@ -2,9 +2,4 @@
 export POSTGRES_USER=postgres
 export POSTGRES_PASSWORD=postgres
 
-if [ "$STATUS" == "complete" ]; then
-  cd /ecommerce-workshop/deploy
-  docker-compose -f docker-compose-fixed-instrumented.yml up -d\
-  echo "App is up!"
-fi
-
+ls ecommworkshop
