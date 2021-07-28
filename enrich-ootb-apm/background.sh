1020f4348844133e05235e7286527df57cd99d1f
tammergalal <tammer.galal@datadoghq.com>
2021-07-28 11:01:02 -0400

    testing v3 and traffic container

--- enrich-ootb-apm/background.sh
+++ enrich-ootb-apm/background.sh
@@ -6,7 +6,4 @@ cd /ecommworkshop/
 sudo sed -i '38i \ \ \ \ volumes:\n \ \ \ \ \ - "../../discounts-service-fixed:/app"' /ecommworkshop/deploy/docker-composedocker-compose-fixed-instrumented.yml
 sudo sed -i '84i \ \ \ \ volumes:\n \ \ \ \ \ - "../../ads-service-fixed:/app"' /ecommworkshop/deploy/docker-composedocker-compose-fixed-instrumented.yml
 
-/ecommworkshop/gor --input-file-loop --input-file /ecommworkshop/requests_0.gor --output-http "http://localhost:3000" >> /dev/null 2>&1
-
-
 
