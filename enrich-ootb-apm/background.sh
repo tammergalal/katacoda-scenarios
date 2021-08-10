470c0b7cbad06800e9375bb62d2a250be9d9a51b
tammergalal <tammer.galal@datadoghq.com>
2021-08-10 15:35:35 -0400

    fixing background.sh

--- enrich-ootb-apm/background.sh
+++ enrich-ootb-apm/background.sh
@@ -3,7 +3,8 @@ curl -s https://datadoghq.dev/katacodalabtools/r?raw=true|bash
 
 cd /ecommworkshop/
 
-sudo sed -i '38i \ \ \ \ volumes:\n \ \ \ \ \ - "../../discounts-service-fixed:/app"' /ecommworkshop/deploy/docker-compose-fixed-instrumented.yml
-sudo sed -i '84i \ \ \ \ volumes:\n \ \ \ \ \ - "../../ads-service-fixed:/app"' /ecommworkshop/deploy/docker-compose-fixed-instrumented.yml
+sudo sed -i '38i \ \ \ \ volumes:\n \ \ \ \ \ - "../../discounts-service-fixed:/app"' /ecommworkshop/deploy/docker-compose/docker-compose-fixed-instrumented.yml
+sudo sed -i '84i \ \ \ \ volumes:\n \ \ \ \ \ - "../../ads-service-fixed:/app"' /ecommworkshop/deploy/docker-compose/docker-compose-fixed-instrumented.yml
+sudo sed -i '50i \ \ \ \ \ - "- DD_PROFILING_ENABLED=true"' /ecommworkshop/deploy/docker-compose/docker-compose-fixed-instrumented.yml
 
 
