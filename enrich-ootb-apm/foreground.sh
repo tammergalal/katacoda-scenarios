aa7283e7eeb5e0687238de32531e0f7517475296
tammergalal <tammer.galal@datadoghq.com>
2021-08-11 13:18:11 -0400

    fixing error

--- enrich-ootb-apm/foreground.sh
+++ enrich-ootb-apm/foreground.sh
@@ -3,7 +3,7 @@
 export POSTGRES_USER=postgres
 export POSTGRES_PASSWORD=postgres
 cd /ecommworkshop/deploy/docker-compose
-docker-compose -f docker-compose-broken-instrumented up -d
+docker-compose -f docker-compose-broken-instrumented.yml up -d
 docker-compose -f docker-compose-traffic-replay.yml up -d
 clear
 
