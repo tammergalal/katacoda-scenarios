6738ac41f62e17928bdff656490ea9f2bb6b6186
tammergalal <tammer.galal@datadoghq.com>
2021-09-13 13:41:50 -0400

    working on background

--- modern-observability-in-practice/background.sh
+++ modern-observability-in-practice/background.sh
@@ -5,21 +5,14 @@ curl -s https://datadoghq.dev/katacodalabtools/r?raw=true|bash
 touch /root/status.txt
 sleep 1
 STATUS=$(cat /root/status.txt)
+echo ""> /root/status.txt
 
-if [ "$STATUS" != "complete" ]; then
-  echo ""> /root/status.txt
-
-  wall -n "Cloning necessary materials"
-
-  mkdir /ecommerce-observability
-  git clone https://github.com/DataDog/ecommerce-workshop /ecommerce-observability
-  cd ../ecommerce-observability
-
-  echo "complete">>/root/status.txt
-fi
+mkdir /ecommerce-observability
+git clone https://github.com/DataDog/ecommerce-workshop /ecommerce-observability
+cd ../ecommerce-observability
 
 # locked to specific commit on 2020-10-02
 git checkout 9ce34516d9a65d6f09a6fffd5c4911a409d31e3f
 git reset --hard
 
-statusupdate complete
+echo "complete">>/root/status.txt
