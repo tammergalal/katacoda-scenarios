ffd569ef514a3f9e8f479902f28ea86d6b305a00
tammergalal <tammer.galal@datadoghq.com>
2021-09-15 13:09:06 -0400

    testing

--- service-focused-observability/background.sh
+++ service-focused-observability/background.sh
@@ -6,17 +6,17 @@ touch /root/status.txt
 sleep 1
 STATUS=$(cat /root/status.txt)
 
-if [ ! -d "../ecommworkshop" ]; then
-  echo ""> /root/status.txt
-  mkdir /ecommworkshop
-  git clone https://github.com/DataDog/ecommerce-workshop /ecommworkshop
-  cd ../ecommworkshop
 
-  # locked to specific commit on 2020-10-02
-  git checkout 9ce34516d9a65d6f09a6fffd5c4911a409d31e3f
-  git reset --hard
-else
-  echo "complete">>/root/status.txt
-if
+echo ""> /root/status.txt
+mkdir /ecommworkshop
+git clone https://github.com/DataDog/ecommerce-workshop /ecommworkshop
+cd ../ecommworkshop
+
+# locked to specific commit on 2020-10-02
+git checkout 9ce34516d9a65d6f09a6fffd5c4911a409d31e3f
+git reset --hard
+
+echo "complete">>/root/status.txt
+
 
 echo "Ready!"
