ce05689a2d8dd6af78249089c07c8070e24827ca
tammergalal <tammer.galal@datadoghq.com>
2021-10-12 15:45:12 -0400

    😿

--- canary-deploy-tracking/background.sh
+++ canary-deploy-tracking/background.sh
@@ -21,6 +21,7 @@ if [ "$STATUS" != "complete" ]; then
   mv /ecommworkshop/traffic-replay/requests_0.gor /root/requests_0.gor
 
   NNODES=$(kubectl get nodes | grep Ready | wc -l)
+  NPODS=$(kubectl get pods --field-selector=status.phase=Running | grep -v NAME | wc -l)
 
   while [ "$NNODES" != "2" ]; do
     sleep 0.3
@@ -34,7 +35,7 @@ if [ "$STATUS" != "complete" ]; then
   kubectl apply -f k8s-yaml-files/discounts.yaml
   kubectl apply -f k8s-yaml-files/frontend.yaml
 
-  while [ "$NPODS" != "3" ]; do
+  while [ "$NPODS" != "4" ]; do
     sleep 0.3
     NPODS=$(kubectl get pods --field-selector=status.phase=Running | grep -v NAME | wc -l)
   done
