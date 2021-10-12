bc96e264ca64b50de3b64945fe9560b3b9575097
tammergalal <tammer.galal@datadoghq.com>
2021-10-12 16:19:36 -0400

    😻

--- canary-deploy-tracking/background.sh
+++ canary-deploy-tracking/background.sh
@@ -20,23 +20,25 @@ if [ "$STATUS" != "complete" ]; then
   mv /ecommworkshop/traffic-replay/requests_0.gor /root/requests_0.gor
 
   NNODES=$(kubectl get nodes | grep Ready | wc -l)
+  NPODS=$(kubectl get pods --field-selector=status.phase=Running | grep -v NAME | wc -l)
 
   while [ "$NNODES" != "2" ]; do
     sleep 0.3
     NNODES=$(kubectl get nodes | grep Ready | wc -l)
   done
 
+  wall -n "Creating ecommerce deployment"
   kubectl apply -f k8s-yaml-files/db.yaml
   kubectl apply -f k8s-yaml-files/advertisements.yaml
   kubectl apply -f k8s-yaml-files/advertisements-service.yaml
   kubectl apply -f k8s-yaml-files/discounts.yaml
   kubectl apply -f k8s-yaml-files/frontend.yaml
 
-  while [ "$NPODS" != "3" ]; do
+  while [ "$NPODS" != "4" ]; do
     sleep 0.3
     NPODS=$(kubectl get pods --field-selector=status.phase=Running | grep -v NAME | wc -l)
   done
-  
+
   wget -q -O - https://github.com/buger/goreplay/releases/download/v1.1.0/gor_1.1.0_x64.tar.gz | tar -xz -C /usr/local/bin
   echo "complete">>/root/status.txt
 fi
