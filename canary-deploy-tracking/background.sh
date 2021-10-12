4390d8057ed7aedd4e6bfdc99986bfd86701d107
tammergalal <tammer.galal@datadoghq.com>
2021-10-12 16:42:24 -0400

    😿

--- canary-deploy-tracking/background.sh
+++ canary-deploy-tracking/background.sh
@@ -6,46 +6,51 @@ touch /root/status.txt
 sleep 1
 STATUS=$(cat /root/status.txt)
 
-# wall -n "Installing Helm and cloning necessary materials"
-statusupdate installingHelm
+if [ "$STATUS" != "complete" ]; then
+  echo ""> /root/status.txt
 
-curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
-chmod 700 get_helm.sh
-./get_helm.sh
-helm repo add datadog https://helm.datadoghq.com
-helm repo update
-mv /usr/local/bin/gor /root/gor
-mv /ecommworkshop/traffic-replay/requests_0.gor /root/requests_0.gor
+  # wall -n "Installing Helm and cloning necessary materials"
+  statusupdate installingHelm
+
+  curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
+  chmod 700 get_helm.sh
+  ./get_helm.sh
+  helm repo add datadog https://helm.datadoghq.com
+  helm repo update
+  mv /usr/local/bin/gor /root/gor
+  mv /ecommworkshop/traffic-replay/requests_0.gor /root/requests_0.gor
 
 
-NNODES=$(kubectl get nodes | grep Ready | wc -l)
-NPODS=$(kubectl get pods --field-selector=status.phase=Running | grep -v NAME | wc -l)
-
-while [ "$NNODES" != "2" ]; do
-  sleep 0.3
   NNODES=$(kubectl get nodes | grep Ready | wc -l)
-done
-
-while ! [  -f /root/k8s-yaml-files/db.yaml ]; do
-  sleep .5
-done
-
-statusupdate deployment
-# wall -n "Creating ecommerce deployment"
-kubectl apply -f k8s-yaml-files/db.yaml
-kubectl apply -f k8s-yaml-files/advertisements.yaml
-kubectl apply -f k8s-yaml-files/advertisements-service.yaml
-kubectl apply -f k8s-yaml-files/discounts.yaml
-kubectl apply -f k8s-yaml-files/frontend.yaml
-
-statusupdate checkPods
-while [ "$NPODS" != "4" ]; do
-  sleep 0.3
   NPODS=$(kubectl get pods --field-selector=status.phase=Running | grep -v NAME | wc -l)
-done
 
-wget -q -O - https://github.com/buger/goreplay/releases/download/v1.1.0/gor_1.1.0_x64.tar.gz | tar -xz -C /usr/local/bin
-statusupdate complete
+  while [ "$NNODES" != "2" ]; do
+    sleep 0.3
+    NNODES=$(kubectl get nodes | grep Ready | wc -l)
+  done
+
+  while ! [  -f /root/k8s-yaml-files/db.yaml ]; do
+    sleep .5
+  done
+
+  statusupdate deployment
+  # wall -n "Creating ecommerce deployment"
+  kubectl apply -f k8s-yaml-files/db.yaml
+  kubectl apply -f k8s-yaml-files/advertisements.yaml
+  kubectl apply -f k8s-yaml-files/advertisements-service.yaml
+  kubectl apply -f k8s-yaml-files/discounts.yaml
+  kubectl apply -f k8s-yaml-files/frontend.yaml
+
+  statusupdate checkPods
+  while [ "$NPODS" != "4" ]; do
+    sleep 0.3
+    NPODS=$(kubectl get pods --field-selector=status.phase=Running | grep -v NAME | wc -l)
+  done
+
+  wget -q -O - https://github.com/buger/goreplay/releases/download/v1.1.0/gor_1.1.0_x64.tar.gz | tar -xz -C /usr/local/bin
+  statusupdate complete
+
+fi
 
 
 ./gor --input-file-loop --input-file "./requests_0.gor|300%" --output-http "http://localhost:30001" >> /dev/null 2>&1
--- canary-deploy-tracking/foreground.sh
+++ canary-deploy-tracking/foreground.sh
@@ -8,4 +8,4 @@ clear
 statuscheck installingHelm 
 statusupdate checkPods
 statuscheck deployment
-prepenvironment
+# prepenvironment
