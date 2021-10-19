15e8b6754b27a8670acf5e2ebef1b03998774b20
tammergalal <tammer.galal@datadoghq.com>
2021-10-19 17:54:38 -0400

    😻

--- ws-d21-service-focused-observability/background.sh
+++ ws-d21-service-focused-observability/background.sh
@@ -27,7 +27,7 @@ sed -i "s/'analytics_enabled': true, //" ./store-frontend-broken-instrumented/co
 mv /root/docker-compose.yml /root/lab
 wget -q -O - https://github.com/buger/goreplay/releases/download/v1.1.0/gor_1.1.0_x64.tar.gz | tar -xz -C /usr/local/bin
 mv /usr/local/bin/gor /root/lab/gor
-mv /ecommworkshop/traffic-replay/requests_0.gor /root/requests_0.gor
+mv /ecommworkshop/traffic-replay/requests_0.gor /root/lab/requests_0.gor
 
 docker-compose up -d
 ./gor --input-file-loop --input-file "./requests_0.gor|300%" --output-http "http://localhost:3000" >> /dev/null 2>&1
