b731f548bacfbdd165cd5fe96f12b961ceb0c0db
tammergalal <tammer.galal@datadoghq.com>
2022-01-04 11:57:05 -0500

    working on background

--- apmintrov3/instrumentappv3/background.sh
+++ apmintrov3/instrumentappv3/background.sh
@@ -2,4 +2,26 @@
 
 curl -sk https://datadoghq.dev/katacodalabtools/r?raw=true|bash
 
-/ecommworkshop/gor --input-file-loop --input-file /ecommworkshop/requests_0.gor --output-http "http://localhost:3000" >> /dev/null 2>&1
+statusupdate "tools"
+statuscheck "environment"
+
+# Wait for required assets to appear in the filesystem
+until  [ -f /root/dd_agent.sql ]
+do
+  sleep 2
+done
+
+cd /root/lab
+
+sed -i "s/'analytics_enabled': true, //" ./store-frontend-broken-instrumented/config/initializers/datadog.rb
+
+mv /root/docker-compose.yml /root/lab
+wget -q -O - https://github.com/buger/goreplay/releases/download/v1.1.0/gor_1.1.0_x64.tar.gz | tar -xz -C /usr/local/bin
+mv /usr/local/bin/gor /root/lab/gor
+mv /ecommworkshop/traffic-replay/requests_0.gor /root/lab/requests_0.gor
+
+docker-compose up -d
+
+statusupdate "workspace"
+
+./gor --input-file-loop --input-file "./requests_0.gor|350%" --output-http "http://localhost:3000" >> /dev/null 2>&1
