aa9d5a5add6ef845919337158bb8a6c43acfa567
tammergalal <tammer.galal@datadoghq.com>
2022-01-04 11:14:32 -0500

    changing foreground

--- apmintrov3/fixappv3/foreground.sh
+++ apmintrov3/fixappv3/foreground.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
+statuscheck "tools"
 
 export POSTGRES_USER=postgres
 export POSTGRES_PASSWORD=postgres
-cd /ecommworkshop/docker-compose-files
-docker-compose -f docker-compose-broken-instrumented.yml up -d
+
+[ ! -d "/root/lab" ] && mkdir /root/lab 
+
+cd /root/lab
+
+printf "DD_API_KEY=$DD_API_KEY\n\
+DD_APP_KEY=$DD_APP_KEY\n\
+POSTGRES_USER=$POSTGRES_USER\n\
+POSTGRES_PASSWORD=$POSTGRES_PASSWORD\n" > /root/lab/.env 
+
 clear
 
-envready
+statusupdate "environment"
+statuscheck "workspace"
+
 statusupdate complete
-clear
-
 prepenvironment
