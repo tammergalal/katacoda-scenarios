28ab8747df7063a8f8554a582b7e699d001573f0
tammergalal <tammer.galal@datadoghq.com>
2022-01-05 14:19:56 -0500

    adding back in folder...pubkat

---
+++ apmintrov3/fixappv3/foreground.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+statuscheck "tools"
+
+export POSTGRES_USER=postgres
+export POSTGRES_PASSWORD=postgres
+
+[ ! -d "/root/lab" ] && mkdir /root/lab 
+
+cd /root/lab
+
+printf "DD_API_KEY=$DD_API_KEY\n\
+DD_APP_KEY=$DD_APP_KEY\n\
+POSTGRES_USER=$POSTGRES_USER\n\
+POSTGRES_PASSWORD=$POSTGRES_PASSWORD\n" > /root/lab/.env 
+
+clear
+
+statusupdate "environment"
+
+statusupdate complete
+prepenvironment
