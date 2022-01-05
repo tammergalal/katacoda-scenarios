f6342b1edd6226569b28163e20eb1af9f3500a86
tammergalal <tammer.galal@datadoghq.com>
2022-01-05 11:39:17 -0500

    deploying storedog

--- apmintrov3/instrumentappv3/background.sh
+++ apmintrov3/instrumentappv3/background.sh
@@ -5,13 +5,14 @@ curl -sk https://datadoghq.dev/katacodalabtools/r?raw=true|bash
 statusupdate "tools"
 statuscheck "environment"
 
-cd /../../ecommworkshop
+cd /root/lab
 
+mv /root/docker-compose.yml /root/lab
 wget -q -O - https://github.com/buger/goreplay/releases/download/v1.1.0/gor_1.1.0_x64.tar.gz | tar -xz -C /usr/local/bin
 mv /usr/local/bin/gor /root/lab/gor
 mv /ecommworkshop/traffic-replay/requests_0.gor /root/lab/requests_0.gor
 
-docker-compose ../../ecommworkshop/deploy/docker-compose/docker-compose-broken-no-apm-instrumentation.yml up -d
+docker-compose up -d
 
 statusupdate "workspace"
 
