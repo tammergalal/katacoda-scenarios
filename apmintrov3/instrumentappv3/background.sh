c52b395ef5bc492f49f00cb1153237c222bcd0f7
tammergalal <tammer.galal@datadoghq.com>
2022-01-05 10:25:24 -0500

    changing terminal start point

--- apmintrov3/instrumentappv3/background.sh
+++ apmintrov3/instrumentappv3/background.sh
@@ -7,7 +7,6 @@ statuscheck "environment"
 
 cd /root/lab
 
-mv /root/docker-compose.yml /root/lab
 wget -q -O - https://github.com/buger/goreplay/releases/download/v1.1.0/gor_1.1.0_x64.tar.gz | tar -xz -C /usr/local/bin
 mv /usr/local/bin/gor /root/lab/gor
 mv /ecommworkshop/traffic-replay/requests_0.gor /root/lab/requests_0.gor
