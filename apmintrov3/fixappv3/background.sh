d46c79f66ca153176e5ee7cddd491121e486a926
tammergalal <tammer.galal@datadoghq.com>
2022-01-10 12:23:24 -0500

    working on background

--- apmintrov3/fixappv3/background.sh
+++ apmintrov3/fixappv3/background.sh
@@ -13,15 +13,14 @@ done
 
 cd /root/lab
 
-ln -s /ecommworkshop/discounts-service/discounts.py
-ln -s /ecommworkshop/ads-service/ads.py
+# ln -s /ecommworkshop/discounts-service/discounts.py
+# ln -s /ecommworkshop/ads-service/ads.py
 
-ln -s /ecommworkshop/store-frontend-broken-instrumented/app/views/spree/layouts/spree_application.html.erb
-ln -s /ecommworkshop/store-frontend-broken-instrumented/app/views/spree/home/index.html.erb
-ln -s /ecommworkshop/store-frontend-broken-instrumented/app/views/spree/products/show.html.erb
+# ln -s /ecommworkshop/store-frontend-broken-instrumented/app/views/spree/layouts/spree_application.html.erb
+# ln -s /ecommworkshop/store-frontend-broken-instrumented/app/views/spree/home/index.html.erb
+# ln -s /ecommworkshop/store-frontend-broken-instrumented/app/views/spree/products/show.html.erb
 
 mv /root/docker-compose.yml /root/lab
-echo "moved"
 wget -q -O - https://github.com/buger/goreplay/releases/download/v1.1.0/gor_1.1.0_x64.tar.gz | tar -xz -C /usr/local/bin
 mv /usr/local/bin/gor /root/lab/gor
 mv /ecommworkshop/traffic-replay/requests_0.gor /root/lab/requests_0.gor
