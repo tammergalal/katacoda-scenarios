fc7bd6994e1da15f61a6117dff1c9b21065a341b
tammergalal <tammer.galal@datadoghq.com>
2022-01-10 12:45:29 -0500

    checking if updates are going through

--- apmintrov3/fixappv3/background.sh
+++ apmintrov3/fixappv3/background.sh
@@ -1,33 +1,33 @@
-#!/bin/bash
+# #!/bin/bash
 
-curl -sk https://datadoghq.dev/katacodalabtools/r?raw=true|bash
+# curl -sk https://datadoghq.dev/katacodalabtools/r?raw=true|bash
 
-statusupdate "tools"
-statuscheck "environment"
+# statusupdate "tools"
+# statuscheck "environment"
 
-# Wait for required assets to appear in the filesystem
-until  [ -f /root/docker-compose.yml ]
-do
-  echo "DOCKER-COMPOSE!!!!!"
-  sleep 2
-done
+# # Wait for required assets to appear in the filesystem
+# until  [ -f /root/docker-compose.yml ]
+# do
+#   echo "DOCKER-COMPOSE!!!!!"
+#   sleep 2
+# done
 
-cd /root/lab
+# cd /root/lab
 
-# ln -s /ecommworkshop/discounts-service/discounts.py
-# ln -s /ecommworkshop/ads-service/ads.py
+# # ln -s /ecommworkshop/discounts-service/discounts.py
+# # ln -s /ecommworkshop/ads-service/ads.py
 
-# ln -s /ecommworkshop/store-frontend-broken-instrumented/app/views/spree/layouts/spree_application.html.erb
-# ln -s /ecommworkshop/store-frontend-broken-instrumented/app/views/spree/home/index.html.erb
-# ln -s /ecommworkshop/store-frontend-broken-instrumented/app/views/spree/products/show.html.erb
+# # ln -s /ecommworkshop/store-frontend-broken-instrumented/app/views/spree/layouts/spree_application.html.erb
+# # ln -s /ecommworkshop/store-frontend-broken-instrumented/app/views/spree/home/index.html.erb
+# # ln -s /ecommworkshop/store-frontend-broken-instrumented/app/views/spree/products/show.html.erb
 
-mv /root/docker-compose.yml /root/lab
-wget -q -O - https://github.com/buger/goreplay/releases/download/v1.1.0/gor_1.1.0_x64.tar.gz | tar -xz -C /usr/local/bin
-mv /usr/local/bin/gor /root/lab/gor
-mv /ecommworkshop/traffic-replay/requests_0.gor /root/lab/requests_0.gor
+# mv /root/docker-compose.yml /root/lab
+# wget -q -O - https://github.com/buger/goreplay/releases/download/v1.1.0/gor_1.1.0_x64.tar.gz | tar -xz -C /usr/local/bin
+# mv /usr/local/bin/gor /root/lab/gor
+# mv /ecommworkshop/traffic-replay/requests_0.gor /root/lab/requests_0.gor
 
-docker-compose up -d
+# docker-compose up -d
 
-statusupdate "workspace"
+# statusupdate "workspace"
 
-./gor --input-file-loop --input-file "./requests_0.gor|350%" --output-http "http://localhost:3000" >> /dev/null 2>&1
+# ./gor --input-file-loop --input-file "./requests_0.gor|350%" --output-http "http://localhost:3000" >> /dev/null 2>&1
--- apmintrov3/fixappv3/foreground.sh
+++ apmintrov3/fixappv3/foreground.sh
@@ -1,21 +1,23 @@
-#!/bin/bash
-statuscheck "tools"
+# #!/bin/bash
 
-export POSTGRES_USER=postgres
-export POSTGRES_PASSWORD=postgres
+echo "HELLO!!!"
+# statuscheck "tools"
 
-[ ! -d "/root/lab" ] && mkdir /root/lab 
+# export POSTGRES_USER=postgres
+# export POSTGRES_PASSWORD=postgres
 
-cd /root/lab
+# [ ! -d "/root/lab" ] && mkdir /root/lab 
 
-printf "DD_API_KEY=$DD_API_KEY\n\
-DD_APP_KEY=$DD_APP_KEY\n\
-POSTGRES_USER=$POSTGRES_USER\n\
-POSTGRES_PASSWORD=$POSTGRES_PASSWORD\n" > /root/lab/.env 
+# cd /root/lab
 
-clear
+# printf "DD_API_KEY=$DD_API_KEY\n\
+# DD_APP_KEY=$DD_APP_KEY\n\
+# POSTGRES_USER=$POSTGRES_USER\n\
+# POSTGRES_PASSWORD=$POSTGRES_PASSWORD\n" > /root/lab/.env 
 
-statusupdate "environment"
+# clear
 
-statusupdate complete
-prepenvironment
+# statusupdate "environment"
+
+# statusupdate complete
+# prepenvironment
