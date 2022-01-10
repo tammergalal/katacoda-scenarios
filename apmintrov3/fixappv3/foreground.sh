7e9429e358ac3bc49fab95ed4224e31f44e7ecc9
tammergalal <tammer.galal@datadoghq.com>
2022-01-10 12:58:02 -0500

    working on index.json

--- apmintrov3/fixappv3/foreground.sh
+++ apmintrov3/fixappv3/foreground.sh
@@ -7,13 +7,13 @@ export POSTGRES_PASSWORD=postgres
 [ ! -d "/root/lab" ] && mkdir /root/lab 
 
 cd /root/lab
+mv /root/docker-compose.yml /root/lab
 
 printf "DD_API_KEY=$DD_API_KEY\n\
 DD_APP_KEY=$DD_APP_KEY\n\
 POSTGRES_USER=$POSTGRES_USER\n\
 POSTGRES_PASSWORD=$POSTGRES_PASSWORD\n" > /root/lab/.env 
 
-clear
 
 statusupdate "environment"
 
