22072f30a4096e7b0d1b3f7bcb10df7f1f345b42
tammergalal <tammer.galal@datadoghq.com>
2022-01-11 12:50:53 -0500

    working on docker-compose

--- apmintrov3/fixappv3/background.sh
+++ apmintrov3/fixappv3/background.sh
@@ -11,18 +11,13 @@ do
   sleep 2
 done
 
-
 cd /root/lab
 
 ln -s /ecommworkshop/discounts-service/discounts.py
-ln -s /ecommworkshop/discounts-service/requirements.txt
 ln -s /ecommworkshop/ads-service/ads.py
-
 ln -s /ecommworkshop/store-frontend-broken-instrumented/app/views/spree/layouts/spree_application.html.erb
 ln -s /ecommworkshop/store-frontend-broken-instrumented/app/views/spree/home/index.html.erb
 ln -s /ecommworkshop/store-frontend-broken-instrumented/app/views/spree/products/show.html.erb
-ln -s /ecommworkshop/store-frontend-broken-instrumented/config/initializers/datadog.rb
-ln -s /ecommworkshop/store-frontend-broken-instrumented/Gemfile
 
 mv /root/docker-compose.yml /root/lab
 wget -q -O - https://github.com/buger/goreplay/releases/download/v1.1.0/gor_1.1.0_x64.tar.gz | tar -xz -C /usr/local/bin
@@ -30,5 +25,7 @@ mv /usr/local/bin/gor /root/lab/gor
 mv /ecommworkshop/traffic-replay/requests_0.gor /root/lab/requests_0.gor
 
 docker-compose up -d
+
+statusupdate "workspace"
 
 ./gor --input-file-loop --input-file "./requests_0.gor|350%" --output-http "http://localhost:3000" >> /dev/null 2>&1
