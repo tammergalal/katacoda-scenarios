ceb1dc087fdcf3f378f2584f6b15c2942e60a072
tammergalal <tammer.galal@datadoghq.com>
2022-01-14 10:26:58 -0500

    background

--- apmintrov3/instrumentappv3/background.sh
+++ apmintrov3/instrumentappv3/background.sh
@@ -24,6 +24,12 @@ ln -s /ecommworkshop/store-frontend-broken-instrumented/Gemfile
 
 mv /root/docker-compose.yml /root/lab
 wget -q -O - https://github.com/buger/goreplay/releases/download/v1.1.0/gor_1.1.0_x64.tar.gz | tar -xz -C /usr/local/bin
+
+until  [ -f /usr/local/bin/gor ]
+do
+  sleep 2
+done
+
 mv /usr/local/bin/gor /root/lab/gor
 mv /ecommworkshop/traffic-replay/requests_0.gor /root/lab/requests_0.gor
 
