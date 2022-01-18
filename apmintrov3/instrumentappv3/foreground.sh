3f6d21acd86d7e6f97a57f7606fc43af943b1eb0
tammergalal <tammer.galal@datadoghq.com>
2022-01-18 13:41:33 -0500

    working with puppeteer

--- apmintrov3/instrumentappv3/foreground.sh
+++ apmintrov3/instrumentappv3/foreground.sh
@@ -5,22 +5,22 @@ export POSTGRES_USER=postgres
 export POSTGRES_PASSWORD=postgres
 export STOREDOG_URL=https://[[HOST_SUBDOMAIN]]-3000-[[KATACODA_HOST]].environments.katacoda.com
 
+clear
+statusupdate "environment"
+statuscheck "workspace"
+
 [ ! -d "/root/lab" ] && mkdir /root/lab 
 
 cd /root/lab
 
 printf "DD_API_KEY=$DD_API_KEY\n\
 DD_APP_KEY=$DD_APP_KEY\n\
-DD_APPLICATION_ID=$DD_APPLICATION_ID\n\
-DD_CLIENT_TOKEN=$DD_CLIENT_TOKEN\n\
 POSTGRES_USER=$POSTGRES_USER\n\
 POSTGRES_PASSWORD=$POSTGRES_PASSWORD\n\
-STOREDOG_URL=$STOREDOG_URL" > /root/lab/.env 
+STOREDOG_URL=$STOREDOG_URL" > .env 
 
 clear
 
-statusupdate "environment"
-statuscheck "workspace"
-
 statusupdate complete
+start
 prepenvironment
