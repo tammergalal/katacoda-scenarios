9b490b4f4c598951d733948f3551814331edfe2f
tammergalal <tammer.galal@datadoghq.com>
2022-01-20 11:55:55 -0500

    working on edits to get puppeteer added

--- apmintrov3/fixappv3/background.sh
+++ apmintrov3/fixappv3/background.sh
@@ -6,7 +6,7 @@ statusupdate "tools"
 statuscheck "environment"
 
 # Wait for required assets to appear in the filesystem
-until  [ -f /root/dd_agent.sql ]
+until  [ -f /root/puppeteer.sh ]
 do
   sleep 2
 done
@@ -20,12 +20,7 @@ ln -s /ecommworkshop/store-frontend-broken-instrumented/app/views/spree/home/ind
 ln -s /ecommworkshop/store-frontend-broken-instrumented/app/views/spree/products/show.html.erb
 
 mv /root/docker-compose.yml /root/lab
-wget -q -O - https://github.com/buger/goreplay/releases/download/v1.1.0/gor_1.1.0_x64.tar.gz | tar -xz -C /usr/local/bin
-mv /usr/local/bin/gor /root/lab/gor
-mv /ecommworkshop/traffic-replay/requests_0.gor /root/lab/requests_0.gor
 
 docker-compose up -d
 
 statusupdate "workspace"
-
-./gor --input-file-loop --input-file "./requests_0.gor|350%" --output-http "http://localhost:3000" >> /dev/null 2>&1
